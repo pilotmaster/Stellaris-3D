dcdb104ccd7409da8851ea705bf3af2b4b7a10b5
pilotmaster - Kyle Hobdey <pilot_master@hotmail.co.uk>
2015-02-22 11:17:01 +0000

    Minor Changes

--- Stellaris 3D Engine/Stellaris 3D Engine/Code/Main/Main.cpp
+++ Stellaris 3D Engine/Stellaris 3D Engine/Code/Main/Main.cpp
@@ -9,5 +9,5 @@
 //------------------------------------------------------------------------------------
 int WINAPI WinMain(__in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in LPSTR lpCmdLine, __in int nShowCmd)
 {
-
+	
 }
