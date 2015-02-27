3b0152f17d17228d90234a48b565ec7ae8a129af
pilotmaster - Kyle Hobdey <pilot_master@hotmail.co.uk>
2015-02-27 12:24:32 +0000

    Accessors & Mutators for Entity class

--- Stellaris 3D Engine/Stellaris 3D Engine/Code/Entity/Entity.h
+++ Stellaris 3D Engine/Stellaris 3D Engine/Code/Entity/Entity.h
@@ -2,6 +2,8 @@
 // FILE:    Entity.h -----------------------------------------------------------------
 //------------------------------------------------------------------------------------
 
+// TODO: LOCAL MOVEMENT
+
 #ifndef _ENTITY_H_
 #define _ENTITY_H_
 
@@ -32,10 +34,149 @@ public:
 
 	// ACCESSORS
 	//---------------------------------
+	inline void GetPosition(DirectX::XMFLOAT3& outPosition)
+	{
+		outPosition = mPosition;
+	}
+
+	inline float GetXPosition()
+	{
+		return mPosition.x;
+	}
+
+	inline float GetYPosition()
+	{
+		return mPosition.y;
+	}
+
+	inline float GetZPosition()
+	{
+		return mPosition.z;
+	}
+
+	inline void GetRotation(DirectX::XMFLOAT3& outRotation)
+	{
+		outRotation = mRotation;
+	}
+
+	inline float GetXRotation()
+	{
+		return mRotation.x;
+	}
+
+	inline float GetYRotation()
+	{
+		return mRotation.y;
+	}
+
+	inline float GetZRotation()
+	{
+		return mRotation.z;
+	}
+
+	inline void GetScale(DirectX::XMFLOAT3& outScale)
+	{
+		outScale = mScale;
+	}
+
+	inline float GetXScale()
+	{
+		return mScale.x;
+	}
+
+	inline float GetYScale()
+	{
+		return mScale.y;
+	}
+
+	inline float GetZScale()
+	{
+		return mScale.z;
+	}
 
 
 	// MUTATORS
 	//---------------------------------
+	inline void SetPosition(DirectX::XMFLOAT3& inPosition)
+	{
+		mPosition = inPosition;
+	}
+
+	inline void Move(DirectX::XMFLOAT3& inAmount)
+	{
+		mPosition.x += inAmount.x;
+		mPosition.y += inAmount.y;
+		mPosition.z += inAmount.z;
+	}
+
+	inline void SetXPosition(float inX)
+	{
+		mPosition.x = inX;
+	}
+
+	inline void SetYPosition(float inY)
+	{
+		mPosition.y = inY;
+	}
+
+	inline void SetZPosition(float inZ)
+	{
+		mPosition.z = inZ;
+	}
+
+	inline void SetRotation(DirectX::XMFLOAT3& inRotation)
+	{
+		mRotation = inRotation;
+	}
+
+	inline void Rotate(DirectX::XMFLOAT3& inAmount)
+	{
+		mRotation.x += inAmount.x;
+		mRotation.y += inAmount.y;
+		mRotation.z += inAmount.z;
+	}
+
+	inline void SetXRotation(float inX)
+	{
+		mRotation.x = inX;
+	}
+
+	inline void SetYRotation(float inY)
+	{
+		mRotation.y = inY;
+	}
+
+	inline void SetZRotation(float inZ)
+	{
+		mRotation.z = inZ;
+	}
+
+	inline void SetScale(DirectX::XMFLOAT3& inScale)
+	{
+		mScale = inScale;
+	}
+
+	inline void Scale(DirectX::XMFLOAT3& inAmount)
+	{
+		mScale.x += inAmount.x;
+		mScale.y += inAmount.y;
+		mScale.z += inAmount.z;
+	}
+
+	inline void SetXScale(float inX)
+	{
+		mScale.x = inX;
+	}
+
+	inline void SetYScale(float inY)
+	{
+		mScale.y = inY;
+	}
+
+	inline void SetZScale(float inZ)
+	{
+		mScale.z = inZ;
+	}
 
 
 private:
@@ -48,7 +189,7 @@ private:
 	//---------------------------------
 	DirectX::XMFLOAT3 mPosition;
 	DirectX::XMFLOAT3 mRotation;
-
+	DirectX::XMFLOAT3 mScale;
 };
 
 
