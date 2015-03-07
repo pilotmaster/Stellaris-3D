0980c3d832add2926d903285f27bf730cbeaa8d4
pilotmaster - Kyle Hobdey <pilot_master@hotmail.co.uk>
2015-03-07 20:00:00 +0000

    Added rotating light Changed default texture of cube model

--- Stellaris 3D Engine/Stellaris 3D Engine/Code/Main/Main.cpp
+++ Stellaris 3D Engine/Stellaris 3D Engine/Code/Main/Main.cpp
@@ -22,6 +22,13 @@ int WINAPI WinMain(__in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, _
 	
 	// SCENE SET UP
 	//---------------------------------
+	// movement variables
+	const float MOVE_SPEED = 50.0f;
+	const float ROTATE_SPEED = 150.0f;
+	const float ORBIT_RADIUS = 20.0f;
+	const float ORBIT_SPEED = 0.5f;
+
+	// Model loading
 	sge::CCamera* camMain = pEngine->CreateCamera(DirectX::XMFLOAT3(-15.0f, 20.0f, -40.0f));
 
 	sge::CMesh* mshCube = pEngine->LoadMesh("Media\\Cube.x");
@@ -30,12 +37,13 @@ int WINAPI WinMain(__in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, _
 	sge::CMesh* mshFloor = pEngine->LoadMesh("Media\\Floor.x");
 	sge::CModel* mdlFloor = pEngine->CreateModel(mshFloor);
 
-	// movement variables
-	const float MOVE_SPEED = 50.0f;
-	const float ROTATE_SPEED = 150.0f;
+	sge::CMesh* mshLight = pEngine->LoadMesh("Media\\Light.x");
+	sge::CModel* mdlLight1 = pEngine->CreateModel(mshLight);
+	sge::CModel* mdlLight2 = pEngine->CreateModel(mshLight, DirectX::XMFLOAT3(-20.0f, 30.0f, 50.0f));
 
 	// frame time variable
 	float delta = 0.0f; sge::CTimer::GetTimerInstace()->GetDeltaTime();
+	float rotation = 0.0f;
 
 
 	// MAIN GAME LOOP
@@ -46,8 +54,14 @@ int WINAPI WinMain(__in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, _
 		//---------------------------------
 		pEngine->Render(camMain);
 
-		// Update delta time 
+		// Update delta time & light orbit rotation
 		delta = sge::CTimer::GetTimerInstace()->GetDeltaTime();
+		rotation -= ORBIT_SPEED * delta;
+
+
+		// UPDATE MODEL POSITIONS
+		//---------------------------------
+		mdlLight1->SetPosition(DirectX::XMFLOAT3(cosf(rotation) * ORBIT_RADIUS, 10.0f, sinf(rotation) * ORBIT_RADIUS));
 
 
 		// CHECK FOR KEY PRESSES
--- Stellaris 3D Engine/Stellaris 3D Engine/Media/Cube.x
+++ Stellaris 3D Engine/Stellaris 3D Engine/Media/Cube.x
@@ -133,7 +133,7 @@ Mesh {
       0.000000;0.000000;0.000000;;
 
       TextureFilename {
-        "wood2.jpg";
+        "StoneDiffuseSpecular.dds";
       }
     }
   }
